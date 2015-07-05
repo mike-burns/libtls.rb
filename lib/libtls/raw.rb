aa67c25cec29c8f8718141afa10147e28258defe
Mike Burns <mike@mike-burns.com>
2015-07-05 23:11:36 +0200

    Fill in all remaining functions
    These functions are untested, and it says so in the documentation.
    Hopefully a kind soul will contribute a test or two for them.

--- lib/libtls/raw.rb
+++ lib/libtls/raw.rb
@@ -111,11 +111,32 @@ module Raw
   #   freed by calling +tls_config_free+.
   #
   #   @param config [FFI::Pointer] the TLS config
-  #   @return void
+  #   @return [nil] +void+
   attach_function :tls_config_free, [:pointer], :void
   # @!endgroup
 
-  # TODO: tls_config_parse_protocols
+  ##
+  # @!method tls_config_parse_protocols(protocols, protostr)
+  #
+  #   Parse a protocol string into a bitmask
+  #
+  #   The +tls_config_parse_protocols+ function parses a protocol string and
+  #   returns the corresponding value via the +protocols+ argument. This value
+  #   can then be passed to the {#tls_config_set_protocols} function. The
+  #   protocol string is a comma or colon separated list of keywords. Valid
+  #   keywords are +tlsv1.0+, +tlsv1.1+, +tlsv1.2+, +all+ (all supported
+  #   protocols), +default+ (an alias for +secure+), +legacy+ (an alias for
+  #   +all+) and +secure+ (currently TLSv1.2 only). If a value has a negative
+  #   prefix (in the form of a leading exclamation mark) then it is removed
+  #   from the list of available protocols, rather than being added to it.
+  #
+  #   @param protocols [FFI::Pointer] a +uint32_t+ pointer to store the parse
+  #     result
+  #   @param protostr [String] the protocol string
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_parse_protocols, [:uint32_t, :string], :int
 
   ##
   # @!group Client configuration
@@ -133,10 +154,42 @@ module Raw
   #     certificates
   #   @return [Fixnum] 0 on success, -1 on error
   attach_function :tls_config_set_ca_file, [:pointer, :string], :int
-  # @!endgroup
 
-  # TODO: tls_config_set_ca_path
-  # TODO: tls_config_set_ca_mem
+  ##
+  # @!method tls_config_set_ca_path(config, ca_path)
+  #
+  #   Use the directory specified to find the root certificate file
+  #
+  #   +tls_config_set_ca_path+ sets the path (directory) which should be
+  #   searched for root certificates.
+  #
+  #   This applies to clients.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param ca_path [String] absolute path to the directory containing the
+  #     root certificates
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_ca_path, [:pointer, :string], :int
+
+  ##
+  # @!method tls_config_set_ca_mem(config, cert, len)
+  #
+  #   Use the root certicate from memory
+  #
+  #   +tls_config_set_ca_mem+ sets the root certificates directly from memory.
+  #
+  #   This applies to clients.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param cert [FFI::Pointer] uint8_t pointer to the cert
+  #   @param len [Fixnum] number of bytes in the cert
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_ca_mem, [:pointer, :pointer, :size_t], :int
+  # @!endgroup
 
   ##
   # @!group Client and server configuration
@@ -154,9 +207,25 @@ module Raw
   #     certificate
   #   @return [Fixnum] 0 on success, -1 on error
   attach_function :tls_config_set_cert_file, [:pointer, :string], :int
-  # @!endgroup
 
-  # TODO: tls_config_set_cert_mem
+  ##
+  # @!method tls_config_set_cert_mem(config, cert, len)
+  #
+  #   Use the given file as the public certificate
+  #
+  #   +tls_config_set_cert_mem+ sets the public certificate directly from
+  #   memory.
+  #
+  #   This applies to clients and servers.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param cert [FFI::Pointer] uint8_t pointer to the cert
+  #   @param len [Fixnum] number of bytes in the cert
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_cert_mem, [:pointer, :pointer, :size_t], :int
+  # @!endgroup
 
   ##
   # @!group Client and server configuration
@@ -174,11 +243,38 @@ module Raw
   attach_function :tls_config_set_ciphers, [:pointer, :string], :int
   # @!endgroup
 
-  # TODO: tls_config_set_dheparams
-  # TODO: tls_config_set_ecdhecurve
-
   ##
   # @!group Server configuration
+  # @!method tls_config_set_dheparams(config, params)
+  #
+  #   Tune the dheparams
+  #
+  #   This applies to servers.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param params [String] one of "none" (0), "auto" (-1), or "legacy"
+  #     (1024). The default is "none".
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_dheparams, [:pointer, :string], :int
+
+  ##
+  # @!method tls_config_set_ecdhecurve(config, name)
+  #
+  #   Use the specified EC DHE curve
+  #
+  #   This applies to servers.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param name one of "none" (+NID_undef+), "auto" (-1), or any NID value
+  #     understood by OBJ_txt2nid (3).
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_ecdhecurve, [:pointer, :string], :int
+
+  ##
   # @!method tls_config_set_key_file(config, key_file)
   #
   #   Set the private key via an absolute file path
@@ -192,9 +288,24 @@ module Raw
   #   @param key_file [String] absolute path to a private key in a file
   #   @return [Fixnum] 0 on success, -1 on error
   attach_function :tls_config_set_key_file, [:pointer, :string], :int
-  # @!endgroup
 
-  # TODO: tls_config_set_key_mem
+  ##
+  # @!method tls_config_set_key_mem(config, key, len)
+  #
+  #   Set the private key via a value in memory
+  #
+  #   +tls_config_set_key_mem+ directly sets the private key from memory.
+  #
+  #   This applies to servers.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param key [FFI::Pointer] a uint8_t pointer to the key
+  #   @param len [Fixnum] number of bytes in the key
+  #   @return [Fixnum] 0 on success, -1 on error
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_key_mem, [:pointer, :pointer, :size_t], :int
+  # @!endgroup
 
   ##
   # @!group Client and server configuration
@@ -221,12 +332,115 @@ module Raw
   attach_function :tls_config_set_protocols, [:pointer, :uint], :void
   # @!endgroup
 
-  # TODO: tls_config_set_verify_depth
-  # TODO: tls_config_clear_keys
-  # TODO: tls_config_insecure_noverifycert
-  # TODO: tls_config_insecure_noverifyname
-  # TODO: tls_config_verify
-  # TODO: tls_load_file
+  ##
+  # @!group Client configuration
+  # @!method tls_config_set_verify_depth(config, verify_depth)
+  #
+  #   Set the maximum depth for the certificate chain
+  #
+  #   See SSL_CTX_set_verify_depth(3) for details.
+  #
+  #   This applies to clients.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @param verify_depth [Fixnum] the maximum depth for certificate chain
+  #     verification
+  #   @return [nil] +void+
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_set_verify_depth, [:pointer, :int], :void
+  # @!endgroup
+
+  ##
+  # @!group Server configuration
+  # @!method tls_config_clear_keys(config)
+  #
+  #   Clear secret keys from memory
+  #
+  #   +tls_config_clear_keys+ clears any secret keys from memory.
+  #
+  #   This applies to servers.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @return [nil] +void+
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_config_clear_keys, [:pointer], :void
+  # @!endgroup
+
+  ##
+  # @!group Client configuration
+  # @!method tls_config_insecure_noverifycert(config)
+  #
+  #   Insecurely disable certficate verification
+  #
+  #   +tls_config_insecure_noverifycert+ disables certificate verification. Be
+  #   extremely careful when using this option.
+  #
+  #   This applies to clients.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @return [nil] +void+
+  #
+  #   @note This method should not be used, and is therefore untested.
+  attach_function :tls_config_insecure_noverifycert, [:pointer], :void
+
+  ##
+  # @!method tls_config_insecure_noverifyname(config)
+  #
+  #   Insecurely disable server name verification
+  #
+  #   +tls_config_insecure_noverifyname+ disables server name verification. Be
+  #   careful when using this option.
+  #
+  #   This applies to clients.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @return [nil] +void+
+  #
+  #   @note This method should not be used, and is therefore untested.
+  attach_function :tls_config_insecure_noverifyname, [:pointer], :void
+
+  ##
+  # @!method tls_config_verify(config)
+  #
+  #   Restore default verification
+  #
+  #   +tls_config_verify+ reenables server name and certificate verification.
+  #
+  #   This applies to clients.
+  #
+  #   @param config [FFI::Pointer] the TLS config
+  #   @return [nil] +void+
+  #
+  #   @note While this method is fine, getting to this state is not. This
+  #     method is therefore untested.
+  attach_function :tls_config_verify, [:pointer], :void
+  # @!endgroup
+
+  ##
+  # @!group Client and server configuration
+  # @!method tls_load_file(file, len, password)
+  #
+  #   Load a certificate or key
+  #
+  #   +tls_load_file+ loads a certificate or key from disk into memory to be
+  #   loaded with {#tls_config_set_ca_mem}, {#tls_config_set_cert_mem} or
+  #   {#tls_config_set_key_mem}. A private key will be decrypted if the
+  #   optional +password+ argument is specified.
+  #
+  #   This applies to clients and servers.
+  #
+  #   @param file [String] the absolute filename
+  #   @param len [FFI::Pointer] pointer to a +size_t+ storing the number of
+  #     bytes loaded
+  #   @param password [String] either +FFI::Pointer::NULL+ or the password for
+  #     the private key
+  #   @return [FFI::Pointer] pointer to +uint8_t+, the key
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_load_file, [:string, :pointer, :string], :pointer
+  # @!endgroup
 
   ##
   # @!group Create, prepare, and free a connection context
@@ -276,7 +490,16 @@ module Raw
   #   @return [Fixnum] 0 on success, -1 on error
   attach_function :tls_configure, [:pointer, :pointer], :int
 
-  # TODO: tls_reset
+  ##
+  # @!method tls_reset(ctx)
+  #
+  #   Reset a context to a newly-initialized state
+  #
+  #   @param ctx [FFI::Pointer] a TLS context
+  #   @return [nil] +void+
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_reset, [:pointer], :void
 
   ##
   # @!method tls_close(ctx)
@@ -338,10 +561,96 @@ module Raw
   #     {TLS_WRITE_AGAIN}, the caller should repeat the call.
   attach_function :tls_connect, [:pointer, :string, :string], :int
 
-  # TODO: tls_connect_fds
-  # TODO: tls_connect_servername
-  # TODO: tls_connect_socket
-  # TODO: tls_accept_fds
+  ##
+  # @!method tls_connect_fds(ctx, fd_read, fd_write, servername)
+  #
+  #   Upgrade an existing connection to secure
+  #
+  #   +tls_connect_fds+ connects a client context to a pair of existing file
+  #   descriptors.
+  #
+  #   @param ctx [FFI::Pointer] a TLS context
+  #   @param fd_read [Fixnum] the read file descriptor
+  #   @param fd_write [Fixnum] the write file descriptor
+  #   @param servername [String] the name of the server, as matched in the
+  #     certificate
+  #   @return [Fixnum] 0 on success, -1 on error, {TLS_READ_AGAIN} if a read
+  #     opreation is necessary to continue, {TLS_WRITE_AGAIN} if a write
+  #     operation is necessary to continue. In case of a {TLS_READ_AGAIN} or
+  #     {TLS_WRITE_AGAIN}, the caller should repeat the call.
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_connect_fds, [:pointer, :int, :int, :string], :int
+
+  ##
+  # @!method tls_connect_servername(ctx, host, port, servername)
+  #
+  #   Open a TLS connection to the +host+ and +port+, verifying against
+  #   +servername+
+  #
+  #   The +tls_connect_servername+ function has the same behaviour as
+  #   {#tls_connect}, however the name to use for verification is explicitly
+  #   provided, rather than being inferred from the host value.
+  #
+  #   @param ctx [FFI::Pointer] a TLS context
+  #   @param host [String] the server to connect to, as an IPv4 address, an
+  #     IPv6 address, anything that can be resolved by +getaddrinfo+.
+  #   @param port [String] the port as a number, service name, or NULL pointer.
+  #     If it is a NULL pointer, the host is parsed for the port number.
+  #   @param servername [String] the server name to verify the certificate
+  #     against
+  #   @return [Fixnum] 0 on success, -1 on error, {TLS_READ_AGAIN} if a read
+  #     opreation is necessary to continue, {TLS_WRITE_AGAIN} if a write
+  #     operation is necessary to continue. In case of a {TLS_READ_AGAIN} or
+  #     {TLS_WRITE_AGAIN}, the caller should repeat the call.
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_connect_servername,
+    [:pointer, :string, :string, :string], :int
+
+  ##
+  # @!method tls_connect_socket(ctx, s, servername)
+  #
+  #   Upgrade an existing socket
+  #
+  #   +tls_connect_socket+ connects a client context to an already established
+  #   socket connection.
+  #
+  #   @param ctx [FFI::Pointer] a TLS context
+  #   @param s [Fixnum] the file descriptor for a socket
+  #   @param servername [String] the server name to verify the certificate
+  #     against
+  #   @return [Fixnum] 0 on success, -1 on error, {TLS_READ_AGAIN} if a read
+  #     opreation is necessary to continue, {TLS_WRITE_AGAIN} if a write
+  #     operation is necessary to continue. In case of a {TLS_READ_AGAIN} or
+  #     {TLS_WRITE_AGAIN}, the caller should repeat the call.
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_connect_socket, [:pointer, :int, :string], :int
+
+  ##
+  # @!method tls_accept_fds(ctx, cctx, fd_read, fd_write)
+  #
+  #   Perform the TLS handshake on an established pair of file descriptors
+  #
+  #   +tls_accept_fds+ creates a new context suitable for reading and writing
+  #   on an existing pair of file descriptors and returns it in +*cctx+. A
+  #   configured server context should be passed in +ctx+ and +*cctx+ should be
+  #   initialized to NULL.
+  #
+  #   @see #tls_accept_socket
+  #
+  #   @param ctx [FFI::Pointer] a TLS context
+  #   @param cctx [FFI::Pointer] a reference to a TLS context
+  #   @param fd_read [Fixnum] the read file descriptor
+  #   @param fd_write [Fixnum] the write file descriptor
+  #   @return [Fixnum] 0 on success, -1 on error, {TLS_READ_AGAIN} if a read
+  #     opreation is necessary to continue, {TLS_WRITE_AGAIN} if a write
+  #     operation is necessary to continue. In case of a {TLS_READ_AGAIN} or
+  #     {TLS_WRITE_AGAIN}, the caller should repeat the call.
+  #
+  #   @todo This is untested. Please {https://github.com/mike-burns/libtls.rb#contributing contribute a patch}.
+  attach_function :tls_accept_fds, [:pointer, :pointer, :int, :int], :int
 
   ##
   # @!method tls_accept_socket(ctx, cctx, socket)
